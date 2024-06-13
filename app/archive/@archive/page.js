c4e730c65ce9a1c0023f8e2611eff7eb781673dc
herno <herno.russo@gmail.com>
2024-06-13 12:01:57 -0300

    Move archive header to the layout

--- app/archive/@archive/page.js
+++ app/archive/@archive/page.js
@@ -5,18 +5,15 @@ const ArchivePage = () => {
   const years = getAvailableNewsYears();
 
   return (
-    <>
-      <h1>News Archive</h1>
-      <nav id="archive-header">
-        <ul>
-          {years.map((year) => (
-            <li key={year}>
-              <Link href={`/archive/${year}`}>{year}</Link>
-            </li>
-          ))}
-        </ul>
-      </nav>
-    </>
+    <nav id="archive-header">
+      <ul>
+        {years.map((year) => (
+          <li key={year}>
+            <Link href={`/archive/${year}`}>{year}</Link>
+          </li>
+        ))}
+      </ul>
+    </nav>
   );
 };
 
