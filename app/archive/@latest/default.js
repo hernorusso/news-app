2f69f542db23eafa3aec3f7950bc99822683a5b4
herno <herno.russo@gmail.com>
2024-06-13 12:01:27 -0300

    Add content to latest page

--- app/archive/@latest/default.js
+++ app/archive/@latest/default.js
@@ -1,3 +1,14 @@
-const LatestPage = () => <h1>Latest Page</h1>;
+import { getLatestNews } from "@/lib/news-utils";
+import { NewsList } from "@/app/(news-list)";
+
+const LatestPage = () => {
+  const news = getLatestNews();
+  return (
+    <>
+      <h1>Latest News</h1>
+      <NewsList news={news} />
+    </>
+  );
+};
 
 export default LatestPage;
