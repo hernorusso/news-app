afb728165d36c0abb7b2f0055793bdf5f300b264
herno <herno.russo@gmail.com>
2024-06-13 14:36:50 -0300

    show content based on year and moth

--- app/archive/@archive/[[...filter]]/page.js
+++ app/archive/@archive/[[...filter]]/page.js
@@ -1,27 +1,57 @@
 import Link from "next/link";
 
 import { NewsList } from "@/app/(news-list)";
-import { getNewsForYear, getAvailableNewsYears } from "@/lib/news-utils";
+import {
+  getNewsForYear,
+  getAvailableNewsYears,
+  getAvailableNewsMonths,
+  getNewsForYearAndMonth,
+} from "@/lib/news-utils";
+
+const YEAR_SELECTOR = 0;
+const MONTH_SELECTOR = 1;
 
 const FilteredNewsPage = ({ params: { filter } }) => {
   console.log(filter);
 
-  const years = getAvailableNewsYears();
+  let news;
+  let links = getAvailableNewsYears();
+  const selectedYear = filter?.[YEAR_SELECTOR];
+  const selectedMonth = filter?.[MONTH_SELECTOR];
+
+  if (selectedYear) {
+    news = getNewsForYear(selectedYear);
+    links = getAvailableNewsMonths(selectedYear);
+  }
+
+  if (selectedMonth) {
+    news = getNewsForYearAndMonth(selectedYear, selectedMonth);
+    links = undefined;
+  }
+
+  const content = news?.length > 0;
+
   return (
     <>
       <header id="archive-header">
         <h1>News Archive</h1>
         <nav>
           <ul>
-            {years.map((year) => (
-              <li key={year}>
-                <Link href={`/archive/${year}`}>{year}</Link>
-              </li>
-            ))}
+            {links?.map((link) => {
+              const href = selectedYear
+                ? `/archive/${selectedYear}/${link}`
+                : `/archive/${link}`;
+              return (
+                <li key={link}>
+                  <Link href={href}>{link}</Link>
+                </li>
+              );
+            })}
           </ul>
         </nav>
       </header>
-      <NewsList news={[]} />
+      {content && <NewsList news={news} />}
+      {!content && <p>Content not Found!</p>}
     </>
   );
   // const news = getNewsForYear(year);
