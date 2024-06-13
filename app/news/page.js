2e3072097cf921aaf6d4db3cabb24af3adc39032
herno <herno.russo@gmail.com>
2024-06-13 08:38:16 -0300

    fix link components layout

--- app/news/page.js
+++ app/news/page.js
@@ -5,15 +5,21 @@ const NewsPage = () => (
     <h1>News Page</h1>
     <main>
       <ul>
-        <Link href="news/article-1">
-          <li>Article 1</li>
-        </Link>
-        <Link href="news/article-2">
-          <li>Article 2</li>
-        </Link>
-        <Link href="news/article-3">
-          <li>Article 3</li>
-        </Link>
+        <li>
+          <Link href="news/article-1">
+            <p>Article 1</p>
+          </Link>
+        </li>
+        <li>
+          <Link href="news/article-2">
+            <p>Article 2</p>
+          </Link>
+        </li>
+        <li>
+          <Link href="news/article-3">
+            <p>Article 3</p>
+          </Link>
+        </li>
       </ul>
     </main>
   </>
