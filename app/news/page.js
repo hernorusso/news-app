9b1cf8807396a82c9e3d59b3a8a567d0c4405132
herno <herno.russo@gmail.com>
2024-06-13 12:06:40 -0300

    Refactor neew page

--- app/news/page.js
+++ app/news/page.js
@@ -1,22 +1,14 @@
 import Link from "next/link";
 
 import { DUMMY_NEWS } from "@/dummy-news";
+import { NewsList } from "@/app/(news-list)";
 
 const NewsPage = () => {
   return (
     <>
       <h1>News Page</h1>
       <main>
-        <ul className="news-list">
-          {DUMMY_NEWS.map((news) => (
-            <li key={news.id}>
-              <Link href={`news/${news.slug}`}>
-                <img src={`images/news/${news.image}`} />
-                <p>{news.title}</p>
-              </Link>
-            </li>
-          ))}
-        </ul>
+        <NewsList news={DUMMY_NEWS} />
       </main>
     </>
   );
