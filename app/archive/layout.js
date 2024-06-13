462a6deb32789f0dd8fbe165fac8379f229562a3
herno <herno.russo@gmail.com>
2024-06-13 11:41:28 -0300

    remove header from layout

--- app/archive/layout.js
+++ app/archive/layout.js
@@ -1,9 +1,6 @@
 const ArchiveLayout = ({ archive, latest }) => (
   <>
     <main>
-      <header>
-        <h1>Archive Page</h1>
-      </header>
       <section id="archive-filter">{archive}</section>
       <section id="archive-latest">{latest}</section>
     </main>
