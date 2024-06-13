063d37e8935487a3f0d0f7370a3befa46eea4325
herno <herno.russo@gmail.com>
2024-06-13 11:40:37 -0300

    add navigation to the archive section

--- app/archive/@archive/page.js
+++ app/archive/@archive/page.js
@@ -1,3 +1,23 @@
-const ArchivePage = () => <h1>Archive Page</h1>;
+import Link from "next/link";
+import { getAvailableNewsYears } from "@/lib/news-utils";
+
+const ArchivePage = () => {
+  const years = getAvailableNewsYears();
+
+  return (
+    <>
+      <h1>News Archive</h1>
+      <nav id="archive-header">
+        <ul>
+          {years.map((year) => (
+            <li key={year}>
+              <Link href={`/archive/${year}`}>{year}</Link>
+            </li>
+          ))}
+        </ul>
+      </nav>
+    </>
+  );
+};
 
 export default ArchivePage;
