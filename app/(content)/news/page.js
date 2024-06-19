5d649cfc36bc489c0dd20e37f7ba3761e404a341
herno <herno.russo@gmail.com>
2024-06-19 09:43:05 -0300

    Fetch data from the backend

--- app/(content)/news/page.js
+++ app/(content)/news/page.js
@@ -1,12 +1,47 @@
-import { DUMMY_NEWS } from "@/dummy-news";
+"use client";
+
 import { NewsList } from "@/app/(news-list)";
+import { useEffect, useState } from "react";
 
 const NewsPage = () => {
+  const [news, setNews] = useState();
+  const [isLoading, setIsLoading] = useState(false);
+  const [hasError, setHasError] = useState(false);
+
+  useEffect(() => {
+    const getNews = async () => {
+      setIsLoading(true);
+      try {
+        const response = await fetch("http://localhost:8080/news");
+        if (!response.ok) {
+          setIsLoading(false);
+        }
+        const news = await response.json();
+        setIsLoading(false);
+        setNews(news);
+      } catch (e) {
+        setHasError(true);
+      }
+    };
+
+    getNews();
+  }, []);
+
+  if (isLoading) {
+    return <p>Loading...</p>;
+  }
+  if (hasError) {
+    return <p>Error</p>;
+  }
+
+  if (!news) {
+    return null;
+  }
   return (
     <>
       <h1>News Page</h1>
       <main>
-        <NewsList news={DUMMY_NEWS} />
+        <NewsList news={news} />
       </main>
     </>
   );
