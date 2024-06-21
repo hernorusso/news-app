d5a6f87fad3b36acdc3eaeddc4c877c908abae45
herno <herno.russo@gmail.com>
2024-06-21 13:49:33 -0300

    Outsource modal backdrop That way, we turn the modal component into a server one

--- app/(content)/news/[slug]/@modal/(.)image/page.js
+++ app/(content)/news/[slug]/@modal/(.)image/page.js
@@ -1,17 +1,16 @@
-"use client";
 import { DUMMY_NEWS } from "@/dummy-news";
-import { useRouter } from "next/navigation";
+
+import { ModalBackdrop } from "./modal-backdrop";
 
 const InterceptedNewsImagePage = ({ params: { slug } }) => {
   const news = DUMMY_NEWS.find((news) => news.slug === slug);
-  const router = useRouter();
 
   if (!news) notFound();
   const { title, image } = news;
 
   return (
     <>
-      <div className="modal-backdrop" onClick={router.back} />
+      <ModalBackdrop />
       <dialog className="modal" open>
         <div className="fullscreen-image">
           <img src={`/images/news/${image}`} alf={title} />
